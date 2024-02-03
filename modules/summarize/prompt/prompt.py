6934e856f0f34f818f60e869c2d49a3033405378
Antonin <antonin.valladeau@gmail.com>
2024-02-03 14:41:35 +0100

    allow-list for .gitignore + youtube smy prompt modified

--- modules/summarize/prompt/prompt.py
+++ modules/summarize/prompt/prompt.py
@@ -10,15 +10,17 @@ You have a lot of freedom to make this work well.
 1. You assign a number to each file and append the original file name in-between parentheses (eg. 1 (ewrhEW12v3mdewmn)),
 into a section called FILES TO ANALYZE.
 
-2. You extract a summary of each file provided to you in 20 words or less, into a section called SUMMARY x, where x represents the number of the file.
+2. You extract a summary of each file provided to you in 20 words or less, into a section called SUMMARY FILE x, where x represents the number of the file.
 Each file should have its own summary.
 
 3. You extract the top 10 ideas for all files in a section called MAIN IDEAS.
 You must indicates between parentheses the number of the file in which the idea is taken from.
 
-4. You extract the top 5 quotes for each file in a section called QUOTES. Format the quotes (capitalize the first letter, add a period at the end)
+4. You extract the top 5 quotes for each file in a section called QUOTES.
+You must indicates between parentheses the number of the file in which the quote is taken from.
+Format the quotes (capitalize the first letter, add a period at the end)
 
-5. You combine all understanding of all the files into a single, 50-word max paragraph in a section called SUMMARY ALL FILES.
+5. You combine all understanding of all the files into a single, 50-word max paragraph in a section called SUMMARY of ALL FILES.
 
 6. You create a clear-cut one sentence definition of all the technical terms discussed in a section called TERMS DISCUSSED.
 You must indicates between parentheses the number of the file in which the technical term is used.
